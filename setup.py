9c392936b8892531c83490810eb3b9cfb4fef4f7
Rami Bououni <rami.bououni@microsoft.com>
2023-07-03 11:18:52 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md") as file:
+    read_me_description = file.read()
+
+setuptools.setup(
+    name="my-package",
+    version="1.0",
+    author="Dev",
+    description="Demo package",
+    url="url",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
